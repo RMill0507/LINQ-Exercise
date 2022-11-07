2c54fa13877fd80f0582ef7ee0e4947fd40d81c6
Rob <Rmilla445@gmail.com>
2022-11-07 13:00:59 -0500

    Length of Strings order

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> golfBrands = new List<string>() {"Callaway", "Nike", "TaylorMade", "Ping", "Vice", "Bridgestone", "Puma", "Slazenger" };
+            IEnumerable<string> longGolfBrand = golfBrands.OrderBy(x => x.Length);
+            foreach (var i in longGolfBrand)
+            {
+                Console.WriteLine(i);   
+            }
+            
+            
+
         }
     }
 }
